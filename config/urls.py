6bf99f5ac0678f66fc7d49c02603353f0b799bf2
ramses132 <yugo132@gmail.com>
2019-05-28 16:14:00 -0500

    [FIX] - project - required login

--- config/urls.py
+++ config/urls.py
@@ -1,27 +1,31 @@
 from django.conf import settings
-from django.urls import include, path
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.views.generic import TemplateView
+from django.contrib.auth.decorators import login_required
+from django.urls import include, path
 from django.views import defaults as default_views
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
     path(
-        "about/", TemplateView.as_view(template_name="pages/about.html"), name="about"
-    ),
+        "",
+        login_required(TemplateView.as_view(template_name="pages/home.html")),
+        name="home"),
+    path(
+        "about/",
+        TemplateView.as_view(template_name="pages/about.html"),
+        name="about"),
     # Django Admin, use {% url 'admin:index' %}
     path(settings.ADMIN_URL, admin.site.urls),
     # User management
     path("users/", include("challenge.users.urls", namespace="users")),
     path("accounts/", include("allauth.urls")),
     # Your stuff: custom urls includes go here
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(
+    settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if 'rosetta' in settings.INSTALLED_APPS:
-    urlpatterns += [
-        path("rosetta/", include('rosetta.urls'))
-    ]
+    urlpatterns += [path("rosetta/", include('rosetta.urls'))]
 
 if settings.DEBUG:
     # This allows the error pages to be debugged during development, just visit
@@ -47,4 +51,5 @@ if settings.DEBUG:
     if "debug_toolbar" in settings.INSTALLED_APPS:
         import debug_toolbar
 
-        urlpatterns = [path("__debug__/", include(debug_toolbar.urls))] + urlpatterns
+        urlpatterns = [path("__debug__/", include(debug_toolbar.urls))
+                      ] + urlpatterns
